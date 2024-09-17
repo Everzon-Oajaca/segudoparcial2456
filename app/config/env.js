ab4668f3fc7b54f721b53f2603092435a65a7d76
Everzon-Oajaca <eoajacac@miumg.edu.gt>
2024-09-17 13:34:39 -0600

    Practica-ExamenParcial-2
    Practica-1

---
+++ app/config/env.js
@@ -0,0 +1,17 @@
+
+
+const env = {
+  database: 'segudoparcial2456',
+  username: 'segudoparcial2456_user',
+  password: 'y64UUspA3kEe7nUnh9uZfk0nvpBLAqf2',
+  host: 'dpg-crkcvipu0jms73bk1oag-a.oregon-postgres.render.com',
+  dialect: 'postgres',
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
+
+module.exports = env;
